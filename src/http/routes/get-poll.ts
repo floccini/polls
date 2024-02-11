8addbe00b088d0240998e45b67ad04b6f31dd1f1
Giovanni Fossati <giovannifossati11@gmail.com>
2024-02-11 09:56:14 -0300

    feat(redis): add redis support to count poll votes

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import z from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:id", async (request, reply) => {
@@ -24,6 +25,35 @@ export async function getPoll(app: FastifyInstance) {
       },
     });
 
-    return reply.send({ poll: poll });
+    if (!poll) {
+      return reply.status(400).send({ message: "Poll not found." });
+    }
+
+    // Returns an array, in which every even position in the array is the option ID, and the following position is its score
+    const result = await redis.zrange(poll.id, 0, -1, "WITHSCORES");
+
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        // Assign the score by the next position of the array
+        const score = result[index + 1];
+
+        Object.assign(obj, { [line]: Number(score) });
+      }
+      return obj;
+    }, {} as Record<string, number>);
+
+    return reply.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: option.id in votes ? votes[option.id] : 0,
+          };
+        }),
+      },
+    });
   });
 }
